aa493c8df9720f030728cdd245d4ae26f3481317
FADI BAKER <133148112+FadiBaker92@users.noreply.github.com>
2024-05-26 05:34:26 +0000

    did the api call, parsed it and fetched all the needed info

--- app.rb
+++ app.rb
@@ -10,5 +10,11 @@ end
 post("/astronomy_pic") do
   
   nasa_url = "https://api.nasa.gov/planetary/apod?api_key=#{ENV["NASA_KEY"]}"
+  raw_result = HTTP.get(nasa_url ).to_s
+  parsed_result = JSON.parse(raw_result)
+  @title = parsed_result.fetch("title") 
+  @explanation = parsed_result.fetch("explanation")
+  @pic_url = parsed_result.fetch("url")
+
   erb(:process_form)
 end
